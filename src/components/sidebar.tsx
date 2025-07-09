397943b369c5ade4e22e698c4361be95a513cbbc
Yayan-thebest <monedgard@yahoo.com>
2025-07-08 20:00:57 -0400

    6: sidebar and navbar layout

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { DottedSeparator } from './dotted-separator';
+import { Navigation } from './navigation';
+
+export const Sidebar = () => {
+  return (
+    <aside className='h-full bg-neutral-100 p-4 w-full'>
+      <Link href="/" className='flex items-center gap-2'>
+        <Image src="/logo.svg" alt='logo' width={28} height={28}/>
+        <span className='font-medium'>TaskFlow</span>
+      </Link>
+      <DottedSeparator className='my-4'/>
+      <Navigation />
+    </aside>
+  )
+};
