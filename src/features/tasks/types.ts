863e72d74e477bf59722e9d3ad52241160316fb5
Yayan-thebest <monedgard@yahoo.com>
2025-07-14 16:33:37 -0400

    22: building data table

--- src/features/tasks/types.ts
+++ src/features/tasks/types.ts
@@ -1,7 +1,18 @@
+import { Models } from "node-appwrite";
+
 export enum TasksStatus {
     BACKLOG = "BACKLOG",
     TODO = "TODO",
     IN_PROGRESS= "IN_PROGRESS",
     IN_REVIEW = "IN_REVIEW",
     DONE = "DONE",
+};
+
+export type Task = Models.Document & {
+    name: string;
+    status:  TasksStatus;
+    assigneeId: string;
+    projectId: string;
+    position: number;
+    dueDate: string;
 };
