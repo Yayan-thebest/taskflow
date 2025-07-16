afb57210c2cbe0468f12010c9002ce994d4ee209
Yayan-thebest <monedgard@yahoo.com>
2025-07-16 01:22:45 -0400

    26: building data calendar

---
+++ src/features/tasks/components/event-card.tsx
@@ -0,0 +1,53 @@
+import { Project } from "@/features/projects/types";
+import { TasksStatus } from "../types";
+import { cn } from "@/lib/utils";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+
+interface EventCardProps {
+    id: string;
+    title: string;
+    assignee: any;
+    project: Project;
+    status: TasksStatus    
+};
+
+const statusColorMap: Record<TasksStatus, string> = {
+    [TasksStatus.BACKLOG]: "border-l-pink-500",
+    [TasksStatus.TODO]: "border-l-red-500",
+    [TasksStatus.IN_PROGRESS]: "border-l-yellow-500",
+    [TasksStatus.IN_REVIEW]: "border-l-blue-500",
+    [TasksStatus.DONE]: "border-l-emerald-500",
+};
+
+export const EventCard = ({ id, title, assignee, project, status}: EventCardProps) => {
+
+    const workspaceId = useWorkspaceId();
+    const router = useRouter();
+
+    const onClick = (
+        e: React.MouseEvent<HTMLDivElement>
+    ) => {
+        e.stopPropagation();
+        router.push(`/workspaces/${workspaceId}/tasks/${id}`);
+    };
+    
+    return (
+        <div className="px-2">
+            <div onClick={onClick} className={cn("p-1.5 text-xs bg-white text-primary border rounded-md border-l-8 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition", statusColorMap[status])}>
+                <p>{title}</p>
+                <div className="flex items-center gap-x-1">
+                    <MemberAvatar
+                        name={assignee?.name}
+                    />
+                    <div className="size-1 rounded-full bg-neutral-300" />
+                    <ProjectAvatar 
+                        name={project?.name}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -15,6 +15,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TasksStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
 
@@ -74,8 +75,8 @@ export const TaskViewSwitcher = () => {
                         <TabsContent value="kanban" className="mt-0">
                             <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []} />
                         </TabsContent>                    
-                        <TabsContent value="calendar" className="mt-0">
-                            {JSON.stringify(tasks)}
+                        <TabsContent value="calendar" className="mt-0 h-full pb-4">
+                            <DataCalendar data={tasks?.documents ?? []}/>
                         </TabsContent>
                     </>
                 )}
