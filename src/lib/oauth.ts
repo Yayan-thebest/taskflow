9306c78c57f1ec04c412ef55b4a230fb580b7a69
Yayan-thebest <monedgard@yahoo.com>
2025-07-20 12:54:32 -0400

    revert changes in oauth

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,18 +1,19 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 import { createAdminClient } from "./appwrite";
 
 export async function signUpWithGithub() {
 	const { account } = await createAdminClient();
 
-  //const origin = headers().get("origin");
+  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
-		`${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-		`${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+		`${origin}/oauth`,
+		`${origin}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
@@ -21,12 +22,12 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
 	const { account } = await createAdminClient();
 
-  //const origin = headers().get("origin");
+  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${process.env.NEXT_PUBLIC_APP}/oauth`,
-		`${process.env.NEXT_PUBLIC_APP}/sign-up`,
+		`${origin}/oauth`,
+		`${origin}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
